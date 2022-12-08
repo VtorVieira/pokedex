300c458a93a2d0946fab5ebd64f87c33140bd44c
vtorvieira <vieira.vitor42@gmail.com>
2022-12-08 15:22:29 -0300

    fix: analyzing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import 'express-async-errors';
-// import * as cors from 'cors';
+import * as cors from 'cors';
 import tokenRouter from './routers/token.routes';
 import usersRouter from './routers/users.routes';
 import pokemonsRouter from './routers/pokemon.routes';
@@ -40,7 +40,7 @@ class App {
 
     this.app.use(express.json());
     this.app.use(accessControl);
-    // this.app.use(cors());
+    this.app.use(cors());
   }
 
   public start(PORT: string | number): void {
