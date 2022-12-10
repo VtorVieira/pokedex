4cce834b25dffb7a5e4f7f9d8007ade5786d67ce
vtorvieira <vieira.vitor42@gmail.com>
2022-12-10 13:06:45 -0300

    feat: add name on header component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import logo from '../images/logo.svg'
 import { FiLogOut } from 'react-icons/fi';
+import { verifyToken } from '../services/requests';
 
 function Header() {
+  const [user, setUser] = useState<string>('')
   const navigate = useNavigate();
-  
+
+  /* useEffect para carregamento da primeira lista de pokemons */
+  useEffect(() => {
+    async function getName() {
+      const localToken = JSON.parse(localStorage.getItem('token') as string) || [];
+      const logged = await verifyToken(localToken.token);
+      setUser(logged);
+    }
+    getName();
+  },[]);
+
   const handleClick = () => {
     localStorage.removeItem('token');
     navigate('/form');
@@ -12,11 +25,12 @@ function Header() {
 
   return (
     <nav className="flex justify-between items-center bg-gradient-to-l from-indigo-600 to-blue-300 mt-4 ml-2 mr-2 rounded-md xl:ml-16 xl:mr-16">
-      <div className='w-80 mt-2 mb-2 ml-4'>
+      <div className='flex justify-center items-center w-80 mt-2 mb-2 ml-4'>
         <img
           className='rounded-lg w-20'
           src={logo}
           alt="Logo" />
+        <p className='ml-5 font-bold text-white'>Bem-vindo(a): {user}</p>
       </div>
       <button type="button" onClick={handleClick} className="mr-2 text-white text-2xl font-bold">
         <FiLogOut />
